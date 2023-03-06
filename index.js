b293c1f01ee253d1c583ffbba09aa8e9ca2cbfbd
baomn@nhanh.vn <maingocbao220600@gmail.com>
2023-03-06 23:31:19 +0700

    day code

---
+++ index.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("myNav").style.width = "50%";
+  document.querySelector(".active").style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+  document.querySelector(".active").style.display = "none";
+}
